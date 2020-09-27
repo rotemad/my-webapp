1d139f36e298a20058edd4169eb27994a19d4f7f
rotemad <rotem@MacBook-Pro.local>
2020-09-27 13:42:57 +0300

    5

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello you,Welcome to my webapp"
+    return "Welcome to my webapp"
 
 if __name__ == "__main__":
     app.run(host ='0.0.0.0',debug = True)
